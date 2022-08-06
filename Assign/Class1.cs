77fc5ca64a54b8a3642c666924a9dcfbdbaa81a8
Areola John <areolajohnbayo@outlook.com>
2022-08-06 14:28:24 +0100

    Add project files.

---
+++ Assign/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Family
+    {
+        public abstract string Father();
+        public abstract string Mother();
+        public abstract string Child();
+    }
+    internal class Areola : Family
+    {
+        public override string Father()
+        {
+            return "Fatherfather";
+        }
+        public override string Mother()
+        {
+            return "mothermother";
+        }
+
+        public override string Child()
+        {
+        return "Children";
+        }            
+    }
+
+}
---
+++ Assign/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Areola areola = new Areola();
+
+            Console.WriteLine (areola.Father());
+            Console.WriteLine(areola.Mother());
+            Console.WriteLine(areola.Child());
+        }
+    }
+}
